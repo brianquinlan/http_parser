583d6183664630569f369bfbc84e18308a5069db
Brian Quinlan <bquinlan@google.com>
2021-11-15 12:41:22 -0800

    Add a parsing example.

--- example/example.dart
+++ example/example.dart
@@ -2,9 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:http_parser/http_parser.dart';
 
-void main() {
+Future<void> main() async {
   final date = DateTime.utc(2014, 9, 9, 9, 9, 9);
   print(date); // 2014-09-09 09:09:09.000Z
 
@@ -13,4 +16,19 @@ void main() {
 
   final nowParsed = parseHttpDate(httpDateFormatted);
   print(nowParsed); // 2014-09-09 09:09:09.000Z
+
+  final socket = await Socket.connect("example.com", 80);
+  socket.write("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n");
+  await socket.flush();
+
+  StreamSubscription<ParsedHttpResponse>? subscription;
+  subscription = parseHttpResponseStream(socket).listen((final response) {
+    response.drain();
+    if (response.statusCode != 200) {
+      print("Bad response: ${response.statusCode} ${response.reasonPhrase}");
+    }
+    socket.close();
+    subscription!.cancel();
+  });
+  await subscription.asFuture();
 }
